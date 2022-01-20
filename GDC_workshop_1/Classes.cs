5bd6299848bce25aa983da39327cd18a1996765f
USF GameDev Club <91546845+USFGameDevClub@users.noreply.github.com>
2022-01-20 01:09:26 -0500

    Add project files.

---
+++ GDC_workshop_1/Classes.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * A class is like a blueprint of a specific object
+ * They contain some properties, fields, events, methods, etc. 
+ * A class defines the kinds of data and the functionality their objects will have.
+ */
+class Person
+{
+    private string _name;
+    private int _age;
+    private string _occupation;
+
+    /*
+     * Constructor:
+     * Whenever a class is created, its constructor is called. 
+     * A class may have multiple constructors that take different arguments. 
+     * Constructors enable the programmer to set default values, 
+     * limit instantiation, and write code that is flexible and easy to read
+     */
+    public Person(string name, int age, string occupation)  // pass in variables that you want to be instantiated as arguments
+    {
+        this._name = name;
+        this._age = age;
+        this._occupation = occupation;
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine(this._name + " is sleeping");
+    }
+
+    public string getName()
+    {
+        return this._name;
+    }
+
+    public int getAge()
+    {
+        return this._age;
+    }
+
+    public string getOccupation()
+    {
+        return this._occupation;
+    }
+
+    public void setName(string name)
+    {
+        this._name = name;
+    }
+
+    public void setAge(int age)
+    {
+        this._age=age;
+    }
+
+    public void setOccupation(string occupation)
+    {
+        this._occupation=occupation;
+    }
+}
+
+// Paste code from the main function below into the main function in Functions.cs to test
+/*
+class main
+{
+    static void Main(string[] args)
+    {
+        Person person1 = new Person("Adam", 21, "Software engineer");
+
+        Console.WriteLine(person1.getName());
+        Console.WriteLine(person1.getAge());
+        Console.WriteLine(person1.getOccupation());
+
+        person1.setName("Ritvik");
+        Console.WriteLine(person1.getName());
+        person1.setOccupation("Student");
+        Console.WriteLine(person1.getOccupation());
+        person1.Sleep();
+    }
+}
+    
+*/
---
+++ GDC_workshop_1/Functions.cs
@@ -0,0 +1,84 @@
+﻿class Functions
+{
+
+    /* 
+     * Methods/Functions: 
+     *
+     * A method is a code block that contains a series of statements. 
+     * A program causes the statements to be executed by calling the method and specifying any required method arguments. 
+    */
+
+    public static void printCharacterName(string characterName, int age) // Void: Method returns no value
+    {                                                                    // Static: Belongs to the type itself rather an object instance
+        Console.WriteLine("Your character's name is: " + characterName); // Arguments: information passed to a method that the method can use
+        Console.WriteLine("Your character's age is: " + age);
+    }
+    static int magicCheck(int magStat, int diceRoll) // returns an int value when method is called
+    {
+        return diceRoll + magStat;
+    }
+
+    static void Main(string[] args) // Main method: main entry point for VS when program is run
+    {
+        /*
+         * Variables: Variables are containers for storing data values. 
+         * 
+         * In C#, there are different types of variables (defined with different keywords), for example:
+         * 
+         *     int - stores integers (whole numbers), without decimals, such as 123 or -123
+         *     double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+         *     char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+         *     string - stores text, such as "Hello World". String values are surrounded by double quotes
+         *     bool - stores values with two states: true or false
+         * 
+         * Defining variables:
+         * 
+         * To define a variable in C#, you must atleast provide the data type and name of your variable
+         * 
+        */
+
+
+        int hp = 0;
+        int magStat = 5;
+        string playerName = "Ritvik";
+        bool isDead = false;
+
+        /*
+        Control statements: A control statement is a statement that determines whether other statements will be executed
+        */
+
+        if (hp == 0) // An if statement decides whether to execute another statement, or decides which of two statements to execute
+        {
+            Console.WriteLine(playerName + " is dead");
+        }
+        else
+        {
+            Console.WriteLine(playerName + " is not dead");
+        }
+
+        while (magStat > 0) // while condition in the parentheses is true, code in the block will be run
+        {
+            Console.WriteLine(playerName + " cast a spell!");
+            magStat = magStat - 2;
+        }
+
+
+        for (int i = 0; i < magStat; i++)
+        {
+            Console.WriteLine(playerName + " cast a spell!");
+        }
+
+
+        printCharacterName("Adam", 21);
+        int checkVal = magicCheck(12, 5);
+
+        Console.WriteLine(checkVal);
+
+        Person person1 = new Person("Adam", 21, "Software engineer"); // Instantiating a new object with name 'person1' from Person class
+
+        Console.WriteLine(person1.getName());
+        Console.WriteLine(person1.getAge());
+        Console.WriteLine(person1.getOccupation());
+    }
+
+}
